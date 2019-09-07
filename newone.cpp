2c34485ba348cc3d312d193b6f4677f21b76f7a4
unknown <prabhat.avb@gmail.com>
2019-09-07 12:34:41 +0530

    second change

--- newone.cpp
+++ newone.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main(){
-    
+
     int a = 10;
 
     cout<<"Learning git and github";
@@ -12,5 +12,6 @@ int main(){
     cout<<a;
 
     //This is our first version..
+    //This is our second version..
     return 0;
 }
